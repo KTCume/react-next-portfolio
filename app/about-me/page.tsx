a3a55600b37effa76681949fd7ef0974fb09ee50
KTCume <167701720+KTCume@users.noreply.github.com>
2025-01-20 18:02:37 +0900

    プロフィール編集

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,9 +1,16 @@
 import styles from "./page.module.css";
 
 export default function Page() {
-    return(
+    return (
         <section className={styles.top}>
-        <h1 className={styles.title}>プロフィール</h1>
+            <h1 className={styles.title}>プロフィール</h1>
+            <ul className={styles.profileList}>
+                <li className={styles.profileItem}>名前: 梅井 太貴</li>
+                <li className={styles.profileItem}>年齢: 19歳</li>
+                <li className={styles.profileItem}>専攻: スーパーITエンジニア専攻</li>
+                <li className={styles.profileItem}>趣味: アニメ、マンガ、KPOP</li>
+                <li className={styles.profileItem}>好きな食べ物: ラーメン、ハンバーガー</li>
+            </ul>
         </section>
     );
-}; 
+};
