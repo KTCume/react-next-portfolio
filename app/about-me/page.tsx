4ad8b32d76eda2d8ce47aedaea585c72668b4e06
KTCume <167701720+KTCume@users.noreply.github.com>
2025-01-20 14:12:08 +0900

    プロフィール編集

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -2,6 +2,8 @@ import styles from "./page.module.css";
 
 export default function Page() {
     return(
+        <section>
         <h1 className={styles.title}>プロフィール</h1>
+        </section>
     );
 }; 
