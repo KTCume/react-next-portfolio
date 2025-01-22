3473639036151979692e7392a33190dacdf2a76d
KTCume <167701720+KTCume@users.noreply.github.com>
2025-01-23 00:08:32 +0900

    detail追加

--- app/_components/Widgets/index.tsx
+++ app/_components/Widgets/index.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 import styles from "./index.module.css";
 
 // ウィジェットアイテムコンポーネント
-const WidgetItem: React.FC<{ imageSrc: string; content: string; link: string }> = ({ imageSrc, content, link }) => {
+const WidgetItem: React.FC<{ imageSrc: string; content: string;  detail: string;link: string }> = ({ imageSrc, content, detail, link }) => {
   return (
     <Link href={link} className={styles.widgetItem}>
       <div className={styles.imageWrapper}>
         <img src={imageSrc} alt="widget" className={styles.image} />
       </div>
       <p className={styles.text}>{content}</p>
+      <p className={styles.inside}>{detail}</p>
     </Link>
   );
 };
@@ -18,17 +19,20 @@ const WidgetList: React.FC = () => {
   const items = [
     {
       imageSrc: "blog.jpg", // 画像URL
-      content: "ブログ: 取り組みについて日々追加していきます。",
+      content: "ブログ ",
+      detail: "取り組みについて日々追加していきます。",
       link: "/news", // ニュースリンク
     },
     {
       imageSrc: "pasokon.jpg", // 画像URL
-      content: "スキル一覧: 学習中のスキルを掲載しています。",
+      content: "スキル一覧" ,
+      detail: "学習中のスキルを掲載しています。",
       link: "/skill", // 天気情報リンク
     },
     {
       imageSrc: "webkiso.jpg", // 画像URL
-      content: "カレンダー: 今日の予定を確認しましょう。",
+      content: "カレンダー ",
+      detail: "今日の予定を確認しましょう。",
       link: "/calendar", // カレンダーリンク
     },
   ];
@@ -36,7 +40,7 @@ const WidgetList: React.FC = () => {
   return (
     <div className={styles.widgetList}>
       {items.map((item, index) => (
-        <WidgetItem key={index} imageSrc={item.imageSrc} content={item.content} link={item.link} />
+        <WidgetItem key={index} imageSrc={item.imageSrc} content={item.content}  detail={item.detail} link={item.link} />
       ))}
     </div>
   );
