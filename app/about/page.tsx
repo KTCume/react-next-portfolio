1df453900b202ee87c8d0778347e7bff540b87eb
KTCume <167701720+KTCume@users.noreply.github.com>
2025-01-22 13:48:43 +0900

    about

---
+++ app/about/page.tsx
@@ -0,0 +1,36 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+    return (
+        <div>
+            <section className={styles.top}>
+                <h1 className={styles.title}>プロフィール</h1>
+                <div className={styles.profileContainer}>
+                    <div className={styles.profileItem}>
+                        <span className={styles.profileLabel}>名前:</span> 梅井 太貴
+                    </div>
+                    <div className={styles.divider}></div>
+
+                    <div className={styles.profileItem}>
+                        <span className={styles.profileLabel}>年齢:</span> 19歳
+                    </div>
+                    <div className={styles.divider}></div>
+
+                    <div className={styles.profileItem}>
+                        <span className={styles.profileLabel}>専攻:</span> スーパーITエンジニア専攻
+                    </div>
+                    <div className={styles.divider}></div>
+
+                    <div className={styles.profileItem}>
+                        <span className={styles.profileLabel}>趣味:</span> アニメ、マンガ、KPOP
+                    </div>
+                    <div className={styles.divider}></div>
+
+                    <div className={styles.profileItem}>
+                        <span className={styles.profileLabel}>好きな食べ物:</span> ラーメン、ハンバーガー
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
