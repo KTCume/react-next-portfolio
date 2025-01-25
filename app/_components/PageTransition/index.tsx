44a065b0ad1105eaf03472842754b8a101696bdb
KTCume <167701720+KTCume@users.noreply.github.com>
2025-01-25 21:59:42 +0900

    アニメーション編集

--- app/_components/PageTransition/index.tsx
+++ app/_components/PageTransition/index.tsx
@@ -10,9 +10,9 @@ interface PageTransitionProps {
 export default function PageTransition({ children }: PageTransitionProps) {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.95, y: 50, filter: 'blur(10px)' }}  // 初期状態でぼかしを追加
-      animate={{ opacity: 1, scale: 1, y: 0, filter: 'blur(0px)' }}        // 表示時にぼかしを解除
-      exit={{ opacity: 0, scale: 0.95, y: -50, filter: 'blur(10px)' }}     // 退場時にぼかしを追加
+      initial={{ opacity: 0, scale: 0.95, filter: 'blur(10px)' }}  // 初期状態でぼかしを追加
+      animate={{ opacity: 1, scale: 1, filter: 'blur(0px)' }}      // 表示時にぼかしを解除
+      exit={{ opacity: 0, scale: 0.95, filter: 'blur(10px)' }}     // 退場時にぼかしを追加
       transition={{
         duration: 0.8,
         ease: [0.25, 0.8, 0.25, 1],
