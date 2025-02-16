3010b4079008402f08b2cc2cd3a28f39f38f4de5
KTCume <167701720+KTCume@users.noreply.github.com>
2025-02-17 04:27:16 +0900

    TOPページタイトルの文章量増加/それに伴いアニメーションを調整

--- app/_components/AnimatedText/index.tsx
+++ app/_components/AnimatedText/index.tsx
@@ -7,40 +7,58 @@ const AnimatedText = () => {
   return (
     <div className={styles.body}> {/* CSSモジュールのクラス名を適用 */}
       <div className={styles.container}> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>P</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>O</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>R</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>T</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>F</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>O</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>L</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>I</span> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>O</span> {/* CSSモジュールのクラス名を適用 */}
+        <span className={styles.span}>P</span>
+        <span className={styles.span}>O</span>
+        <span className={styles.span}>R</span>
+        <span className={styles.span}>T</span>
+        <span className={styles.span}>F</span>
+        <span className={styles.span}>O</span>
+        <span className={styles.span}>L</span>
+        <span className={styles.span}>I</span>
+        <span className={styles.span}>O</span>
+      </div>
+
+      <div className={styles.subcontainer}>
+        <span className={styles.subspan}>ポ</span>
+        <span className={styles.subspan}>ー</span>
+        <span className={styles.subspan}>ト</span>
+        <span className={styles.subspan}>フ</span>
+        <span className={styles.subspan}>ォ</span>
+        <span className={styles.subspan}>リ</span>
+        <span className={styles.subspan}>オ</span>
+        <span className={styles.subspan}>サ</span>
+        <span className={styles.subspan}>イ</span>
+        <span className={styles.subspan}>ト</span>
+        <span className={styles.subspan}>へ</span>
+        <span className={styles.subspan}>よ</span>
+        <span className={styles.subspan}>う</span>
+        <span className={styles.subspan}>こ</span>
+        <span className={styles.subspan}>そ</span>
       </div>
 
       {/* 雪のエフェクト */}
-        <div className={styles.snowContainer}>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-            <div className={styles.snow}></div>
-        </div>
+      <div className={styles.snowContainer}>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+        <div className={styles.snow}></div>
+      </div>
     </div>
   );
 };
