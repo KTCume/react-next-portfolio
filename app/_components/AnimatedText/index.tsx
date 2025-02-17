6f16ebb1f51a1d1e0e493d126d6d67bbb101a11f
KTCume <167701720+KTCume@users.noreply.github.com>
2025-02-18 00:59:42 +0900

    コードをさらに改善

--- app/_components/AnimatedText/index.tsx
+++ app/_components/AnimatedText/index.tsx
@@ -6,7 +6,6 @@ import styles from "./index.module.css";
 const AnimatedText = () => {
   return (
     <div className={styles.body}>
-      <div className={styles.starryBackground}></div> {/* 星の背景 */}
       <div className={styles.container}>
         {"Portfolio".split("" ).map((char, index) => (
           <span key={index} className={styles.span} style={{ animationDelay: `${0.8 + index * 0.2}s` }}>
