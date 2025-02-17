2efaef0daac46fe1116011d0bcbfe37af0d6fd91
KTCume <167701720+KTCume@users.noreply.github.com>
2025-02-18 00:38:57 +0900

    コードの書き方をきれいに改善

--- app/_components/AnimatedText/index.tsx
+++ app/_components/AnimatedText/index.tsx
@@ -1,64 +1,25 @@
-"use client"; // クライアントサイドでのみ実行
+"use client";
 
-import React from 'react';
-import styles from './index.module.css'; // CSSモジュールをインポート
+import React, { useEffect } from "react";
+import styles from "./index.module.css";
 
 const AnimatedText = () => {
   return (
-    <div className={styles.body}> {/* CSSモジュールのクラス名を適用 */}
-      <div className={styles.container}> {/* CSSモジュールのクラス名を適用 */}
-        <span className={styles.span}>P</span>
-        <span className={styles.span}>o</span>
-        <span className={styles.span}>r</span>
-        <span className={styles.span}>t</span>
-        <span className={styles.span}>f</span>
-        <span className={styles.span}>o</span>
-        <span className={styles.span}>l</span>
-        <span className={styles.span}>i</span>
-        <span className={styles.span}>o</span>
+    <div className={styles.body}>
+      <div className={styles.starryBackground}></div> {/* 星の背景 */}
+      <div className={styles.container}>
+        {"Portfolio".split("" ).map((char, index) => (
+          <span key={index} className={styles.span} style={{ animationDelay: `${0.8 + index * 0.2}s` }}>
+            {char}
+          </span>
+        ))}
       </div>
-
       <div className={styles.subcontainer}>
-        <span className={styles.subspan}>ご</span>
-        <span className={styles.subspan}>覧</span>
-        <span className={styles.subspan}>い</span>
-        <span className={styles.subspan}>た</span>
-        <span className={styles.subspan}>だ</span>
-        <span className={styles.subspan}>き</span>
-        <span className={styles.subspan}>あ</span>
-        <span className={styles.subspan}>り</span>
-        <span className={styles.subspan}>が</span>
-        <span className={styles.subspan}>と</span>
-        <span className={styles.subspan}>う</span>
-        <span className={styles.subspan}>ご</span>
-        <span className={styles.subspan}>ざ</span>
-        <span className={styles.subspan}>い</span>
-        <span className={styles.subspan}>ま</span>
-        <span className={styles.subspan}>す</span>
-      </div>
-
-      {/* 雪のエフェクト */}
-      <div className={styles.snowContainer}>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
-        <div className={styles.snow}></div>
+        {"ご覧いただきありがとうございます".split("" ).map((char, index) => (
+          <span key={index} className={styles.subspan} style={{ animationDelay: `${0.8 + index * 0.2}s` }}>
+            {char}
+          </span>
+        ))}
       </div>
     </div>
   );
